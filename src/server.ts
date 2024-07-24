1ecdb153c9e0eb5289a62e67a4bad7926b2e16c5
juliogabriel96s <juliogabrielsetor96@gmail.com>
2024-07-24 15:44:37 -0300

    error in app.ts and port server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config'
 import { app } from "./app";
+import { env } from './env';
+
 
 app.listen({
     host: '0.0.0.0',
-    port: 3333
+    port: env.PORT
 }).then(() =>{
     console.log('Server running.')
 })
