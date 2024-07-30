a665cbf4d958fae132a6eaee236e485350692491
juliogabriel96s <juliogabrielsetor96@gmail.com>
2024-07-30 14:41:22 -0300

    add delete and save in tag-repository and in-memory

--- src/repositories/in-memory-repository/in-memory-tag-repository.ts
+++ src/repositories/in-memory-repository/in-memory-tag-repository.ts
@@ -2,6 +2,7 @@ import { Prisma, Tag } from "@prisma/client";
 import { TagRepository } from "../tag-repository";
 
 export class InMemoryTagREpository implements TagRepository{
+    
   
      public Items: Tag[] = []
      public currentId = 1
@@ -37,6 +38,21 @@ export class InMemoryTagREpository implements TagRepository{
         }
 
         return tag
+    }
+
+    async save(tag: Tag){
+        const itemIndex = this.Items.findIndex(item => item.id === tag.id)
+
+        this.Items[itemIndex] = tag
+
+        return tag
+    }
+    async delete(tag: Tag) {
+        const itemIndex =  this.Items.findIndex(item => item.id === tag.id)
+
+        if(itemIndex > -1){
+            this.Items.splice(itemIndex, 1)
+        }
     }
   
 
--- src/repositories/tag-repository.ts
+++ src/repositories/tag-repository.ts
@@ -4,4 +4,6 @@ export interface TagRepository{
     create(data: Prisma.TagCreateInput): Promise<Tag>
     findByName(name: string): Promise<Tag | null>
     findById(id: number): Promise<Tag | null>
+    save(tag: Tag): Promise<Tag>
+    delete(tag: Tag): Promise<void>
 }
